d3217384f289bf1e765b4713ecdc1b301d6d4c31
Will M. Farr <will.farr@ligo.org>
2026-02-12 11:20:58 -0500

    Model structure worked out.

--- model.py
+++ model.py
@@ -5,5 +5,17 @@ import numpyro.distributions as dist
 def cov_func(times, x, T, omega, gamma):
     raise NotImplementedError("Not implemented yet")
 
-def model(times, data, data_err):
-    raise NotImplementedError("Not implemented yet")
+def model(times, data, data_err, omega_bounds, gamma_bounds, T_bounds):
+    A = numpyro.sample('A', dist.Normal(0,1))
+    B = numpyro.sample('B', dist.Normal(0,1))
+
+    x = numpyro.sample('x', dist.Exponential(1))
+    T = numpyro.sample('T', dist.Uniform(*T_bounds))
+
+    omega = numpyro.sample('omega', dist.Uniform(*omega_bounds))
+    gamma = numpyro.sample('gamma', dist.Uniform(*gamma_bounds))
+
+    h_mu = jnp.exp(-gamma*times) * (A * jnp.cos(omega*times) + B * jnp.sin(omega*times))
+    h_cov = cov_func(times, x, T, omega, gamma)
+
+    numpyro.sample('data', dist.MultivariateNormal(h_mu, h_cov + jnp.diag(data_err**2)), obs=data)
