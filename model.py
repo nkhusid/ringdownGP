4490b11f61c857f1d673b97e0ae6d933ef2bab0d
Will M. Farr <will.farr@ligo.org>
2026-02-12 11:15:47 -0500

    Model.py skeleton.

---
+++ model.py
@@ -0,0 +1,9 @@
+import jax.numpy as jnp
+import numpyro
+import numpyro.distributions as dist
+
+def cov_func(times, x, T, omega, gamma):
+    raise NotImplementedError("Not implemented yet")
+
+def model(times, data, data_err):
+    raise NotImplementedError("Not implemented yet")
